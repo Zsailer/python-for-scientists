b93847445b49b5234d9056df01459d33192438f2
lcwheeler <lwheeler9@gmail.com>
2018-05-16 18:55:46 -0600

    move python script to appropriate location

---
+++ examples/plot_sns_hist.py
@@ -0,0 +1,27 @@
+"""
+Histogram
+=============
+
+Example histogram
+"""
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+sns.set()
+
+
+# Initialize a figure and axes object
+fig, ax = plt.subplots(figsize=(3,3))
+
+# Data
+x = np.random.normal(0, 0.1, 1000)
+
+# Add data a scatter points onto axes
+ax.hist(x)
+
+# Name axes
+ax.set_xlabel('x')
+
+# Show figure.
+fig.show()
